4d32468e1f40babe36ee113c83bbba7fab35275c
Mark Old <dlom234@gmail.com>
2013-08-21 14:14:01 -0700

    Running multiple times on multiple gifs will not clobber each-other

--- tableau-gif.sh
+++ tableau-gif.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# you need imagemagick or graphicsmagick
+# you need imagemagick or graphicsmagick, also sed and perl
+# this script will create LOTS of files in the cwd, but it should clean up after itself.  If it doesn't, run `rm ./(gif-filename)-frame*`
 
-convert $1 frame-%06d.png # we wont ever have more than a million frames, will we?
-rm -f ./*.csv
+convert $1 $1-frame-%06d.png # we wont ever have more than a million frames, will we?
+rm -f ./$1*.csv
 
 count=$(($(ls -1 | grep frame | wc -l) - 1))
-echo '"x","y","r","g","b","frame"' >> frames.csv
+echo '"x","y","r","g","b","frame"' >> $1.csv
 for FRAME in `seq -f %06g 0 $count`; do
-    convert frame-$FRAME.png frame-$FRAME.txt
-    sed "s/^\([0-9]\{1,3\}\),\([0-9]\{1,3\}\): ([ ]*\([0-9]\{1,3\}\),[ ]*\([0-9]\{1,3\}\),[ ]*\([0-9]\{1,3\}\).*/\1,\2,\3,\4,\5,$FRAME/g" frame-$FRAME.txt >> frames.csv
+    convert $1-frame-$FRAME.png $1-frame-$FRAME.txt
+    sed "s/^\([0-9]\{1,3\}\),\([0-9]\{1,3\}\): ([ ]*\([0-9]\{1,3\}\),[ ]*\([0-9]\{1,3\}\),[ ]*\([0-9]\{1,3\}\).*/\1,\2,\3,\4,\5,$FRAME/g" $1-frame-$FRAME.txt >> $1.csv # agh
 done
-rm -f ./frame-*
+rm -f ./$1-frame*
 
-perl -p -e 's/\n/\r\n/' < frames.csv > frames_win.csv
+perl -p -e 's/\n/\r\n/' < $1.csv > $1_win.csv # windows line-endings
